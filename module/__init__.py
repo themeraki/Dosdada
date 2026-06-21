90c09f00f4346c208045b58e9a49df1b147aef0f
themeraki <prouser.admin@gmail.com>
2026-06-21 22:41:51 +0530

    Create __init__.py

---
+++ module/__init__.py
@@ -0,0 +1,10 @@
+# modules/__init__.py
+
+# This file treats the directory as a package and 
+# ensures all module functions are exposed for importing.
+
+from . import system
+from . import network
+from . import files
+from . import interaction
+from . import security
